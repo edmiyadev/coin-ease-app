d5f6bb37aea36c206f2ad32ccef65071753bf9d4
Edward Minaya <edmiya.dev@gmail.com>
2025-04-16 00:30:38 -0400

    Add budget data table and columns for budget management

--- src/app/budgets/page.tsx
+++ src/app/budgets/page.tsx
@@ -1,12 +1,73 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
-import { DataTable } from "@/components/data-table";
-import { SectionCards } from "@/components/section-cards";
+import { Budget, columns } from "@/components/budgets/columns";
+import { DataTable } from "@/components/budgets/data-table";
+// import { DataTable } from "@/components/data-table"; // example data-table
 
+// create data for datable
+const data = [
+  {
+    id: "1",
+    amount: 1000,
+    status: "pending",
+    email: "user@email.com",
+  },
+  {
+    id: "2",
+    amount: 2000,
+    status: "success",
+    email: "peppito@gmail.com",
+  },
+  {
+    id: "3",
+    amount: 3000,
+    status: "failed",
+    email: "maria@hotmail.com",
+  },
+];
+
+const budgetData: Budget[] = [
+  {
+    id: "1",
+    budgetAmount: 1500,
+    spentAmount: 1200,
+    remainingAmount: 300,
+    period:  "Enero 2021",
+  },
+  {
+    id: "2",
+    budgetAmount: 600,
+    spentAmount: 550,
+    remainingAmount: 1000,
+    period:  "Enero 2023",
+  },
+  {
+    id: "3",
+    budgetAmount: 400,
+    spentAmount: 380,
+    remainingAmount: 800,
+    period:  "Marzo 2020",
+  },
+  {
+    id: "4",
+    budgetAmount: 200,
+    spentAmount: 250,
+    remainingAmount: 2100,
+    period:  "Abril 2023",
+  },
+  {
+    id: "5",
+    budgetAmount: 300,
+    spentAmount: 290,
+    remainingAmount: 2200,
+    period:  "Abril 2023",
+  },
+]
 
 export default function BudgetsPage() {
   return (
-    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-      <h1>Budgets</h1>
+    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-6">
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={budgetData} />
+      </div>
     </div>
   );
 }
---
+++ src/components/budgets/columns.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "../ui/button";
+import { MoreHorizontal, Pencil, Trash2 } from "lucide-react";
+import { ArrowUpDown } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+type Month =
+  | "Enero"
+  | "Febrero"
+  | "Marzo"
+  | "Abril"
+  | "Mayo"
+  | "Junio"
+  | "Julio"
+  | "Agosto"
+  | "Septiembre"
+  | "Octubre"
+  | "Noviembre"
+  | "Diciembre";
+type Year = number;
+
+type Period = `${Month} ${Year}`;
+
+export type Budget = {
+  id: string;
+  budgetAmount: number;
+  spentAmount: number;
+  remainingAmount: number;
+  period: Period;
+};
+
+export const columns: ColumnDef<Budget>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <div className="flex items-center justify-center">
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="flex items-center justify-center">
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      </div>
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "period",
+    header: () => <div className="text-left">Periodo</div>,
+    cell: ({ row }) => {
+      const period = row.getValue("period") as Period;
+      return <div className="text-left font-medium ">{period}</div>;
+    },
+  },
+  {
+    accessorKey: "budgetAmount",
+    header: () => <div className="text-left">Presupuesto</div>,
+    cell: ({ row }) => {
+      const budgetAmount = parseFloat(row.getValue("budgetAmount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(budgetAmount);
+
+      return <div className="text-left font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "spentAmount",
+    header: () => <div className="text-left">Gastado</div>,
+    cell: ({ row }) => {
+      const spentAmount = parseFloat(row.getValue("spentAmount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(spentAmount);
+
+      return <div className="text-left font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "remainingAmount",
+    header: () => <div className="text-left">Restante</div>,
+    cell: ({ row }) => {
+      const remainingAmount = parseFloat(row.getValue("remainingAmount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(remainingAmount);
+
+      return <div className="text-left font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    header: () => <div className="text-left">Acciones</div>,
+    cell: ({ row }) => {
+      const budget = row.original;
+      
+      return (
+        <div className="flex space-x-2">
+          <Button variant="ghost" className="h-8 w-8 p-0">
+        <span className="sr-only">Editar</span>
+        <Pencil className="h-4 w-4" />
+          </Button>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+        <span className="sr-only">Eliminar</span>
+        <Trash2 className="h-4 w-4" />
+          </Button>
+        </div>
+      );
+        },
+      },
+    ];
